def7fb2ffeb4aa117d217c50561c8ff0ec94a057
Sydney McGee <mcgee.sydney@gmail.com>
2022-08-10 22:16:36 -0500

    Completed comments serializer file + added url paths in urls + added rest framework imports to views file

---
+++ backend/comments/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from comments import views
+
+
+urlpatterns = [
+    path('', views.get_all_comments),
+    path('<str:video_id>/', views.get_all_comments),
+]
--- backend/comments/views.py
+++ backend/comments/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import api_view, permission_classes
+from .models import Comment
+from .serializers import CommentSerializer
+
+
+
+
+
+
 
 # Create your views here.
