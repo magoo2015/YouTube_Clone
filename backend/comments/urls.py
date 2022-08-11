242b088d03e3463aa6ef2489f7d679ba3681c3ed
Sydney McGee <mcgee.sydney@gmail.com>
2022-08-10 22:35:47 -0500

    Made changes to comments url file + added functions in comments view file

--- backend/comments/urls.py
+++ backend/comments/urls.py
@@ -3,6 +3,6 @@ from comments import views
 
 
 urlpatterns = [
-    path('', views.get_all_comments),
     path('<str:video_id>/', views.get_all_comments),
+    path('', views.new_comment),
 ]
--- backend/comments/views.py
+++ backend/comments/views.py
@@ -3,6 +3,8 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.decorators import api_view, permission_classes
+
+from backend.cars import serializers
 from .models import Comment
 from .serializers import CommentSerializer
 
@@ -13,3 +15,18 @@ from .serializers import CommentSerializer
 
 
 # Create your views here.
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def get_all_comments(request, id):
+    comments = Comment.objects.filter(video_id=id)
+    serializer = CommentSerializer(comments, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def new_comment(request):
+    serializer = CommentSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save(user=request.user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
