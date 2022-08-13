73d6e56367a94852a8a37e625c13dc322d3c3f01
Sydney McGee <mcgee.sydney@gmail.com>
2022-08-12 21:59:43 -0500

    Removed null argument from likds & dislikes + update Comment App serializers

--- backend/comments/serializers.py
+++ backend/comments/serializers.py
@@ -5,5 +5,5 @@ from .models import Comment
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
-        fields = ['id', 'video_id', 'text', 'likes', 'dislikes', 'user_id']
+        fields = ['id', 'video_id', 'text', 'likes', 'dislikes', 'user', 'user_id']
         depth = 1
