8d9ab285622c11cf6f19974790cb12996451c021
Sydney McGee <mcgee.sydney@gmail.com>
2022-08-19 15:46:51 -0500

    Currently working on Searchpage.js

--- frontend/src/pages/SearchPage/SearchPage.js
+++ frontend/src/pages/SearchPage/SearchPage.js
@@ -1,2 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import { KEY } from '../../localKey';
+
+
+const SearchPage = () => {
+    
+    const [videoId, setVideoID] = useState();
+    const [searchTerm, setSearch] = useState();
+    const [searchResults, setSearchResults] = useState([]);
+
+    let navigate = useNavigate();
+
+    async function searchVid(searchTerm) {
+        try {
+            let response = await axios.get(
+                `https://www.googleapis.com/youtube/v3/search?q=${searchTerm}&key=${KEY}`);
+
+        } catch (error) {
+            console.log("Error in searchVid");
+        }
+    }
+}
