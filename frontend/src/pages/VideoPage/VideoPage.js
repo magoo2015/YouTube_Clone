383c3da3a4f451fd93fb4789e4d1b32acd8360b6
Sydney McGee <mcgee.sydney@gmail.com>
2022-08-20 15:25:52 -0500

    Commiting current status on VideoPage, testing related videos in postman and implemented it into fetchRelatedVideos function.

--- frontend/src/pages/VideoPage/VideoPage.js
+++ frontend/src/pages/VideoPage/VideoPage.js
@@ -2,3 +2,4 @@ import React, { useState, useEffect } from 'react';
 import  {useParams} from "react-router-dom";
 import { KEY } from "../../localKey";
 import axios from 'axios';
+import { Link } from 'react-router-dom';
