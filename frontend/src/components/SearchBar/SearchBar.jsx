be19107ffe90c2871cd0d951653dc3df9d7078c5
Sydney McGee <mcgee.sydney@gmail.com>
2022-10-07 23:52:57 -0500

    Made some styling adjustments to SearchBar

--- frontend/src/components/SearchBar/SearchBar.jsx
+++ frontend/src/components/SearchBar/SearchBar.jsx
@@ -1,24 +1,30 @@
-import React, { useState } from 'react';
-import {useNavigate} from 'react-router-dom';
-import SearchPage from '../../pages/SearchPage/SearchPage';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import SearchPage from "../../pages/SearchPage/SearchPage";
 
 const SearchBar = (props) => {
-    const [search, updateSearch] = useState();
+  const [search, updateSearch] = useState();
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    function handleSubmit(event) {
-        event.preventDefault();
-        const encoded = encodeURIComponent(search);
-        navigate(`/search/${search}`);
-    }
+  function handleSubmit(event) {
+    event.preventDefault();
+    const encoded = encodeURIComponent(search);
+    navigate(`/search/${search}`);
+  }
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <input type="search" value={search} onChange={(event) => updateSearch(event.target.value)} />
-            <button>Search</button>
-        </form>
-    );
+  return (
+    <div className="search">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="search"
+          value={search}
+          onChange={(event) => updateSearch(event.target.value)}
+        />
+        <button>Search</button>
+      </form>
+    </div>
+  );
 };
 
 export default SearchBar;
